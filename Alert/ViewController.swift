b11348e01548a907c07f667751266fdb9e53dfd3
jkim734 <jkim734@gmail.com>
2021-08-14 01:26:02 +0900

    Initial Commit

---
+++ Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by Kim Jae Hyeon on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
